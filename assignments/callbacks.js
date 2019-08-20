ee3f532d7944e1545c9911967df4b37d77a82286
Eric Richards <esholundr@yahoo.com>
2019-08-20 16:55:41 -0600

    Finished Callbacks.js and Closure.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -38,32 +38,64 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+	// getLength passes the length of the array into the callback.
+	return cb(arr);
 }
+
+const arrLength = arr => {
+	return arr.length;
+};
+console.log(getLength(items, arrLength));
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+	// last passes the last item of the array into the callback.
+	return cb(arr);
 }
+
+const arrLast = arr => {
+	return arr[arr.length - 1];
+};
+console.log(last(items, arrLast));
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+	// sumNums adds two numbers (x, y) and passes the result to the callback.
+	return cb(x, y);
 }
+
+const add = (x, y) => {
+	return x + y;
+};
+console.log(sumNums(4, 2, add));
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+	// multiplyNums multiplies two numbers and passes the result to the callback.
+	return cb(x, y);
 }
 
+const multiply = (x, y) => {
+	return x * y;
+};
+
+console.log(multiplyNums(4, 2, multiply));
+
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+	// contains checks if an item is present inside of the given array/list.
+	// Pass true to the callback if it is, otherwise pass false.
+	return cb(item, list);
 }
+
+const itemCheck = (item, list) => {
+	return list.includes(item, list);
+};
+
+console.log(contains("Pencil", items, itemCheck));
+console.log(contains("Slurm", items, itemCheck));
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+	// removeDuplicates removes all duplicate values from the given array.
+	// Pass the duplicate free array to the callback function.
+	// Do not mutate the original array.
 }
